a78d1ad79ad1258a83bd9c39e6a1d9599c4ec189
Daniel Salamon <ds@pulilab.com>
2015-12-20 13:11:26 +0100

    creating Mentor and Opinion models

--- mentor_candidates/models.py
+++ mentor_candidates/models.py
@@ -2,4 +2,19 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Mentor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+class Opinion(models.Model):
+    mentor = models.ForeignKey(Mentor, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
